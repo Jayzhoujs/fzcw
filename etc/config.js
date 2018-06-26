886e082e5b470dd2cb4f1d73ff2c8805ee382f30
Jayzhoujs <575572498@qq.com>
2018-06-27 07:37:12 +0800

    Add files via upload

---
+++ etc/config.js
@@ -0,0 +1,8 @@
+export default {
+	// 线上环境
+  basePath: 'http://47.106.15.120/api', 
+  domain: 'http://47.106.15.120/', 
+	// 本地环境
+	// basePath: 'http://localhost:3000/api', 
+	// domain: 'http://localhost:3000/', 
+}
